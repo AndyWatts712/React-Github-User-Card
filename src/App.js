eda93da33ca889f96d71b5840aa52cda089ce77b
megawatts68 <andrew.f.watts@gmail.com>
2020-08-05 05:48:26 +0700

    MVP, but it's ughly. Starting to style.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Followers from './components/Followers'
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      githubUser: [],
+      followers: []
+    }
+  }
+
+  componentDidMount() {
+    Axios
+      .get('https://api.github.com/users/andywatts712')
+      .then(res => {
+        this.setState({
+          githubUser: res.data
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    Axios
+      .get('https://api.github.com/users/andywatts712/followers')
+      .then(res => {
+        this.setState({
+          followers: res.data
+        })
+      })
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>{this.state.githubUser.name}</h1>
+        <Followers followers={this.state.followers} />
+      </div>
+    )
+  }
+
 }
-
 export default App;
